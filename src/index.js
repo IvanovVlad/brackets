b6b54962a24cb401d18860fcc90e89ec64f74cc3
IvanovVlad <rr.rr0@mail.ru>
2019-10-16 22:10:05 +0400

    50% solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  console.log(str)
+  console.log(bracketsConfig)
+  if (str.length % 2 != 0) return false;
+
+  let strArray = str.split('');
+  let strlength = strArray.length;
+  for (let i=0; i<strlength; i++)
+  {
+    for (let j=0; j<bracketsConfig.length; j++)
+    {
+        if (strArray[i] == bracketsConfig[j][0] && strArray[i+1] == bracketsConfig[j][1])
+        {
+          strArray = strArray.splice(i, 2);
+          strlength -= 2;
+          console.log(strlength);
+          if (strlength == 0)
+          {
+            return true;
+          }
+          i = 0;
+        }
+      }
+    }
+  return false;
 }
