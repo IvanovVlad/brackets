dbc106cb811af3ed89cc935c5983ecd823ee7083
IvanovVlad <rr.rr0@mail.ru>
2019-10-17 00:04:45 +0400

    Solved

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  console.log(str)
-  console.log(bracketsConfig)
-  if (str.length % 2 != 0) return false;
-
   let strArray = str.split('');
   let strlength = strArray.length;
-  for (let i=0; i<strlength; i++)
-  {
-    for (let j=0; j<bracketsConfig.length; j++)
-    {
-        if ((strArray[i] == bracketsConfig[j][0] && strArray[i+1] == bracketsConfig[j][1]) 
-        || (strArray[i] == bracketsConfig[j][1] && strArray[i+1] == bracketsConfig[j][0]))
-        {
+  for (let i = 0; i < strlength; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (strArray[i] == bracketsConfig[j][0] && strArray[i + 1] == bracketsConfig[j][1]) {
+        strArray.splice(i, 2);
+        strlength -= 2;
+        i = 0;
+      }
+    }
+  }
+  if (strlength == 0) {
+    return true;
+  }
+  else {
+    for (let i = 0; i < strlength; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (strArray[i] == bracketsConfig[j][0] && strArray[i + 1] == bracketsConfig[j][1]){
           strArray.splice(i, 2);
           strlength -= 2;
           i = 0;
         }
       }
     }
-    if (strlength == 0)
-          {
-            return true;
-          }
-          else{
-            return false;
-          }
+    if (strlength == 0) {
+      return true;
+    }
+    return false;
+  }
 }
